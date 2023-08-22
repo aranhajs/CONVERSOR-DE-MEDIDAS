530e2b2dff147c75479e4efd852f2811928d284f
Jonathan Carvalho <125627147+aranhajs@users.noreply.github.com>
2023-08-21 23:45:41 -0300

    Add files via upload

---
+++ EX008.py
@@ -0,0 +1,9 @@
+#CONVERSOR DE MEDIDAS
+
+valor= int(input('Uma distância em Métros: '))
+cm = valor *100
+mm = valor *1000
+print(f'A medida de {valor} Métros, corresponde a: ')
+print(f'{cm}  Centimêtros')
+print(f'{mm} Milimetros')
+
